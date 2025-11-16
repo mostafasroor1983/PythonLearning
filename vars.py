eb10ebdbf01462716a5c4610c2952c3d22920ecb
Mostafa Sroor <mostafa.sroor.1983@gmail.com>
2025-11-16 11:18:48 +0400

    Variables
    Variables

---
+++ vars.py
@@ -0,0 +1,8 @@
+name = "Mostafa"
+print("Hello, " + name + "!")
+print(f"Hello, {name}!")
+print("Hello,", name)
+print("Hello, {}".format(name))
+print("Hello, {}".format(name.upper()))
+print("Hello, {}".format(name.lower()))
+print("Hello, {}".format(name.title())) 
---
+++ vars1.py
@@ -0,0 +1,4 @@
+name = "Mostafa"
+print("This is", name)
+print(name, "is learning Python.")
+print(name, "is enjoying the journey.")
