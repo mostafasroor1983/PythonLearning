ef3113b5c8acd8df26c6a1ba08280be7a4ab18f5
Mostafa Sroor <mostafa.sroor.1983@gmail.com>
2025-11-16 10:50:36 +0400

    Escape examples
    Escape examples

---
+++ escape.py
@@ -0,0 +1,15 @@
+print(" \" Hello World \" ")
+print(' \" Hello World \" ')
+print(" ' Hello World ' ")
+print(" \' Hello World \' ")
+
+print("Hello \n World")
+print("Hello \\ World")
+print("Hello \t World")
+
+print("=====Exercise=====")
+print("Your Learning Path:\n\t- Python Basics.\n\t- Data Engineering.")
+print("""Your Learning Path:
+\t- Python Basics.
+\t- Data Engineering.""")
+
