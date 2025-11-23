cb2567af89d0c4999d4e492e40d115db4371ea60
Mostafa Sroor <mostafa.sroor.1983@gmail.com>
2025-11-23 16:47:06 +0400

    modules
    modules

---
+++ modules/app.py
@@ -0,0 +1,5 @@
+#import convertors
+#print(f"convertors.kg_to_lbs(10): {convertors.kg_to_lbs(10)}")
+
+from convertors import kg_to_lbs
+print(f"kg_to_lbs(10): {kg_to_lbs(10)}")
---
+++ modules/convertors.py
@@ -0,0 +1,24 @@
+def kg_to_lbs(kg):
+    """Convert kilograms to pounds."""
+    return kg * 2.20462
+
+def lbs_to_kg(lbs):
+    """Convert pounds to kilograms."""
+    return lbs / 2.20462
+
+def meters_to_feet(meters):
+    """Convert meters to feet."""
+    return meters * 3.28084
+
+def feet_to_meters(feet):
+    """Convert feet to meters."""
+    return feet / 3.28084
+
+def celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
+    return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius."""
+    return (fahrenheit - 32) * 5/9
+
