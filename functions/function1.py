a4ba0c2fb01d04bd48ace6add72f2bce47305268
Mostafa Sroor <mostafa.sroor.1983@gmail.com>
2025-11-23 09:05:29 +0400

    function 1
    function 1

---
+++ functions/function1.py
@@ -0,0 +1,12 @@
+def greet_user(name):
+    return f"Hello, {name}!"
+
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(a, b):
+    return a * b
+
+print(greet_user("Alice"))
+print(add_numbers(5, 7))
+print(multiply_numbers(3, 4))
